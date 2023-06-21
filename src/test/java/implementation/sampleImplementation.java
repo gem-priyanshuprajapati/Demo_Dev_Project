48ea0b99c241d018be07d9789afc9637f07f74f1
gem-priyanshuprajapati <102684763+gem-priyanshuprajapati@users.noreply.github.com>
2023-06-22 00:52:54 +0530

    Update sampleImplementation.java

--- src/test/java/implementation/sampleImplementation.java
+++ src/test/java/implementation/sampleImplementation.java
@@ -4,8 +4,9 @@ import net.serenitybdd.core.pages.PageObject;
 
 public class sampleImplementation extends PageObject {
     public static void printMsg(){
-        
+    
         System.out.println("Hello world");
+        
     }
 
 }
