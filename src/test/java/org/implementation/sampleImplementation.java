b005e6b95dbb1a4bfd9937bf496692c32ad41093
Priyanshu Prajapati <priyanshu.prajapati@Geminisolutions.com>
2023-07-03 18:28:33 +0530

    Updates

--- src/test/java/org/implementation/sampleImplementation.java
+++ src/test/java/org/implementation/sampleImplementation.java
@@ -3,7 +3,7 @@ package org.implementation;
 public class sampleImplementation {
     public static String testDevjar(){
     
-        return"!!!!!!!!!***********RESPONSE IS FROM DEV_JAR_PRIYANSHU_PRAJAPATI*************** version 1.0.1!!!!!!!!!";
+        return"!!!!!!!!!***********RESPONSE IS FROM DEV_JAR_PRIYANSHU_PRAJAPATI_testing*************** version 1.0.1!!!!!!!!!";
 
     }
 
